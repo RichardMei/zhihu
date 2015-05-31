8b1d0214d4accfd2d1f77825a024b89233cc0f18
RichardMei <richardmeiw@gmail.com>
2015-05-31 20:42:45 +0800

    format

--- zhihu.py
+++ zhihu.py
@@ -18,23 +18,25 @@ r = s.get('http://www.zhihu.com/people/%s/followees' % sys.argv[1], headers=head
 html = BeautifulSoup(r.content)
 followees_num = int(html.find('div',attrs={'class':'zm-profile-side-following zg-clear'}).find('a').strong.string)
 count = (followees_num - 1) / 20 + 1
-
 param =  json.loads(html.find(attrs={'class':'zh-general-list clearfix'})['data-init'])
+
 for i in xrange(count):
-    params = json.dumps({"offset":i*20 , "order_by": "created", "hash_id": param['params']['hash_id']})
+    params = json.dumps({"offset":i * 20 , "order_by": "created", "hash_id": param['params']['hash_id']})
     followlist = s.post('http://www.zhihu.com/node/ProfileFolloweesListV2',\
         data={'method':'next',\
               '_xsrf':html.find(attrs={'name':'_xsrf'})['value'],\
               'params':params}, \
         headers=headers)
     followees = followlist.json()['msg']
-    for j in xrange(len(followees)):
+    
+    size = len(followees)
+    for j in xrange(size)):
         soup = BeautifulSoup(followees[j])
         username = soup.find('a')['title']
         link = soup.find('img')['src']
-        link = link[0:link.index('_')]+link[link.index('_')+2:] 
+        link = link[0:link.index('_')] + link[link.index('_')+2:] 
         img = s.get(link)
-        f = open(username+'.jpg', 'w')
+        f = open(username + '.jpg', 'w')
         f.write(img.content)
         f.close()
         print '%s %s download' % (username,link) 
